c3e6dab343109f0674d0896ed0d6ef40cb90d943
AnkitSinghSenwal <ankitsinghsenwal2001@gmail.com>
2024-06-23 11:16:57 +0530

    1. change all the hyperlinks, 2. and added logic to remove the loader after the questions are loaded.

--- game.js
+++ game.js
@@ -3,6 +3,11 @@ const choices = Array.from( document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText')
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+
+
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -55,6 +60,9 @@ startGame = () => {
     availableQuestions = [...questions];
     console.log(availableQuestions);
     getNewQuestion();
+
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 
